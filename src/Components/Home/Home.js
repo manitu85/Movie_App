b91c6f31674c1742ba179ba0bc3854542f115736
manitu85 <manitu.psy@gmail.com>
2019-09-08 00:27:15 +0200

    #

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react'
-import HeroImage from '../elements/HeroImage/HeroImage';
-import SearchBar from '../elements/SearchBar/SearchBar';
-import FourColGrid from '../elements/FourColGrid/FourColGrid';
-import MovieThumb from '../elements/MovieThumb/MovieThumb';
-import LoadMoreBtn from '../elements/LoadMore/LoadMoreBtn';
-import Spinner from '../elements/Spinner/Spinner';
 import {
   API_URL,
   API_KEY,
@@ -12,15 +6,62 @@ import {
   BACKDROP_SIZE,
   POSTER_SIZE
 } from '../../config.js'
+import HeroImage from '../elements/HeroImage/HeroImage';
+import SearchBar from '../elements/SearchBar/SearchBar';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import MovieThumb from '../elements/MovieThumb/MovieThumb';
+import LoadMoreBtn from '../elements/LoadMore/LoadMoreBtn';
+import Spinner from '../elements/Spinner/Spinner';
+
+
 
 class Home extends Component {
   state = {
-    
+    movies: [],
+    HeroImage: null,
+    loading: false,
+    currentPage: 0,
+    totalPages: 0,
+    searchTerm: ''
   }
+
+  componentDidMount() {
+    this.setState({loading: true})
+
+    const url = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.getData(url);
+  }
+
+
+  getData = url => {
+    fetch(url)
+      .then(result => result.json())
+      .then(data => {
+        this.setState({
+        movies: [...this.state.movies, ...data.results],  // copy movie arr and add new movies
+        HeroImage: this.state.HeroImage || data.results[0]   
+
+      })
+    })
+  }
+
+
+  loadMoreItems = () => {
+    let url = ''; 
+    this.setState({ loading: true })
+
+    if (this.state.searchTerm === '') {
+      url = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.currentPage + 1}`;
+    } else {
+      url = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this.state.searchTerm}&page=${this.state.currentPage + 1}`;
+    }
+    this.getData(url)
+  }
+  
 
   render() {
     return (
-      <div class='rmdb-home'>
+      <div className='rmdb-home'>
         <HeroImage />
         <SearchBar />
         <FourColGrid />
